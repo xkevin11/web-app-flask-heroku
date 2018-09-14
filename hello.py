d9dd66d5af35b3a93e66949331e26ef1f7d47b76
Kev <kevin.wu@infoready.com.au>
2018-09-15 03:04:02 +1000

    added port

--- hello.py
+++ hello.py
@@ -73,7 +73,8 @@ def hi():
 
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 # If without app.run(), need the following to Run:
 # 	export FLASK_APP=hello.py
